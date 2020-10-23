2328acdcc398d53ddab189733e98ee3ed83a8cc1
Mesup Adhikari <72329116+iammesup@users.noreply.github.com>
2020-10-23 00:59:06 -0400

    Update Dictionary exersise.py

--- Dictionary exersise.py
+++ Dictionary exersise.py
@@ -1,3 +1,5 @@
+"""
+Create a dictionary and take input from the user and return the meaning of the word form dictionary """ 
 d1={"coding":"the process of assigning a code to something for classification","python":" a general purpose language","Django": "is a high level python framework","facebook":"social networking site"}
 print("coding")
 print("python")
